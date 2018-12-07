16e79653e55704be6af6eee3558d7a5d6c2c43a2
Christian Fufezan <christian@fufezan.net>
2018-12-07 09:02:46 +0100

    Extended test_jsons

--- test_jsons.py
+++ test_jsons.py
@@ -4,18 +4,84 @@ import glob
 import os
 
 
-def main():
-    for json_file in glob.glob(os.path.join('.','*.json')):
-        print('Testing file: {0}'.format(json_file))
-        try:
-            json.load(open(json_file,'r'))
-            print('\tReading file complete')
-        except Exception as e: 
-            print('\t[ WARNING ]')
-            print('\tError message: {0}'.format(e))
-            print('\tCould not read file!')
-            print('\t[ WARNING ]')
+def uprint(msg, ok=True):
+    if ok:
+        indicator = '[   OK!   ]'
+    else:
+        indicator = '[ WARNING ]'
+    if len(msg) > 65:
+        msg = msg[:62] + '...'
+    print('{0:65}{1}'.format(msg, indicator))
     return
 
+def jsons_are_readable(rcode, jsons):
+    """
+    Read and validate jsons are formatted correctly
+    """
+    local_rcode = 0
+    for json_file in glob.glob(os.path.join('.','*.json')):
+        json_file_basename = os.path.basename(json_file)
+        try:
+            jsons[json_file_basename] = json.load(open(json_file,'r'))
+            uprint('File {0} readable'.format(json_file_basename))
+        except Exception as e:
+            uprint('File {0} corrupt'.format(json_file_basename), ok=False)
+            local_rcode = 1
+    rcode += local_rcode
+    return jsons, rcode
+
+def parameter_ids_are_unique(rcode, jsons):
+    """
+    Check if _ids are unique
+    """
+    local_rcode = 0
+    all_ids = set()
+    for pos, entry in enumerate(jsons['parameters.json']):
+        if entry['_id'] in all_ids:
+            uprint(
+                '{_id} already exists in parameters.json'.format(**entry),
+                ok=False
+            )
+            local_rcode = 2
+        all_ids.add(entry['_id'])
+    if len(all_ids) == pos:
+        uprint('All _ids are unique')
+    rcode += local_rcode
+    return jsons, rcode
+
+
+def styles_are_consistent(rcode, jsons):
+    local_rcode = 0
+    if False:
+        local_rcode = 4
+
+    rcode += local_rcode
+    return jsons, rcode
+
+def styles_have_citation(rcode, jsons):
+    local_rcode = 0
+    for entry in jsons['styles.json']:
+        if len(entry.get('versions', [])) != 0:
+            if len(entry.get('reference', '')) == 0:
+                # Better> could be nice citation regex!
+                uprint('Style {name} haz no reference!'.format(**entry), ok=False)
+                local_rcode = 8
+    if local_rcode == 0:
+        uprint('All styles af references')
+    rcode += local_rcode
+    return jsons, rcode
+
+def main():
+    """
+    Check over all integrity of uparma jsons
+    """
+    rcode = 0 # success
+    jsons = {}
+    jsons, rcode = jsons_are_readable(rcode, jsons)
+    jsons, rcode = parameter_ids_are_unique(rcode, jsons)
+    jsons, rcode = styles_are_consistent(rcode, jsons)
+    jsons, rcode = styles_have_citation(rcode, jsons)
+    return rcode
+
 if __name__ == '__main__':
-    main()
+    exit(main())
