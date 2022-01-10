9171d5dd06646d43806e739e2461dc4faf68803b
tristan-ranff <tristan.ranff@me.com>
2022-01-10 17:24:53 +0100

    added support and test for sorting nested json outermost list is sorted by name innermost objects are not sorted

---
+++ utils/sort.py
@@ -0,0 +1,34 @@
+import json
+
+sortable_types = (list, dict)
+
+
+def dynamic_sort(input, depth=0):
+    if isinstance(input, dict):
+        if any([isinstance(v, sortable_types) for v in input.values()]):
+            return dict(
+                sorted(
+                    {k: dynamic_sort(v, depth + 1) for k, v in input.items()}.items()
+                )
+            )
+    if isinstance(input, list):
+        if any([isinstance(v, sortable_types) for v in input]):
+            next_list = [dynamic_sort(v, depth + 1) for v in input]
+            if depth == 0:
+                return list(sorted(next_list, key=lambda k: k["name"]))
+            elif not any(isinstance(n, dict) for n in next_list):
+                return list(sorted(next_list))
+            else:
+                return next_list
+    return input
+
+
+if __name__ == "__main__":
+    # Write
+    with open("jsons/parameters.json") as f:
+        j = json.load(f)
+    # Sort
+    sorted_j = dynamic_sort(j)
+    # Write
+    with open("jsons/parameters.json", "w") as f:
+        json.dump(sorted_j, f, indent=4)
